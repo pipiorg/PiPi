0e31fbc86aba896e747d56d13b56a80f24c8379a
CH-Chang <ch.chang.software@gmail.com>
2023-12-14 12:46:46 +0800

    feat: add some edit feature

--- src/pipi/PiPiEditor.cpp
+++ src/pipi/PiPiEditor.cpp
@@ -1,12 +1,13 @@
 #include "PiPiEditor.h"
 
 namespace PiPi {
-	PiPiEditor::PiPiEditor(PdfMemDocument* document) {
-		this->init(document);
+	PiPiEditor::PiPiEditor(PdfMemDocument* document, PiPiFontManager* fontManager) {
+		this->init(document, fontManager);
 	}
 
-	void PiPiEditor::init(PdfMemDocument* document) {
+	void PiPiEditor::init(PdfMemDocument* document, PiPiFontManager* fontManager) {
 		this->document = document;
+        this->fontManager = fontManager;
 	}
 
 	bool PiPiEditor::isOperable() {
@@ -71,4 +72,109 @@ namespace PiPi {
 		PdfMemDocument* document = this->document;
 		return this;
 	}
+
+    PiPiEditor* PiPiEditor::setFieldColor(std::string fieldName, int red, int green, int blue) {
+        return this;
+    }
+
+    PiPiEditor* PiPiEditor::setFieldBorderColor(std::string fieldName, int red, int green, int blue) {
+        return this;
+    }
+
+    PiPiEditor* PiPiEditor::setFieldBackgroundColor(std::string fieldName, int red, int green, int blue) {
+        return this;
+    }
+
+    PiPiEditor* PiPiEditor::setFieldTextHorizontalAlignment(std::string fieldName, PiPiTextHorizontalAlignment alignment) {
+        return this;
+    }
+
+    PiPiEditor* PiPiEditor::setFieldMultiline(std::string fieldName, bool multiline) {
+        PdfMemDocument* document = this->document;
+        
+        std::vector<PdfField*>* fields = PiPiUtil::SearchField(document, fieldName);
+        
+        for (auto iterator = fields->begin(); iterator != fields->end(); iterator.operator++()) {
+            PdfField* field = *iterator;
+            
+            PdfFieldType type = field->GetType();
+            if (type != PdfFieldType::TextBox) {
+                continue;
+            }
+            
+            PdfTextBox* textbox = (PdfTextBox*) field;
+
+            textbox->SetMultiLine(multiline);
+        }
+        
+        delete fields;
+        
+        return this;
+    }
+
+    PiPiEditor* PiPiEditor::setFieldFontName(std::string fieldName, std::string fontName) {
+        PdfMemDocument* document = this->document;
+        PiPiFontManager* fontManager = this->fontManager;
+        
+        const PdfFont* font = fontManager->accessFont(fontName);
+        if (font == nullptr) {
+            return;
+        }
+        
+        std::vector<PdfField*>* fields = PiPiUtil::SearchField(document, fieldName);
+        
+        for (auto iterator = fields->begin(); iterator != fields->end(); iterator.operator++()) {
+            PdfField* field = *iterator;
+            
+            PdfDictionary& dictRef = field->GetDictionary();
+            PdfDictionary* dict = &dictRef;
+            
+            PdfObject* daObj = dict->FindKey(PdfName("DA"));
+            if (daObj == nullptr) {
+                continue;
+            }
+            
+            PdfString da = daObj->GetString();
+            std::string daString = da.GetString();
+            
+            std::vector<std::string>* daStringSplits = PiPiCommon::split(daString, " ");
+            std::vector<std::string>* newDaStringSplits = new std::vector<std::string>();
+            
+            for (int i = 0; i < daStringSplits->size(); i++) {
+                if (i == 0) {
+                    newDaStringSplits->push_back("/" + fontName);
+                    continue;
+                }
+                
+                std::string daStringSplit = (*daStringSplits)[i];
+                newDaStringSplits->push_back(daStringSplit);
+            }
+            
+            std::string newDaString = PiPiCommon::join(newDaStringSplits, " ");
+            
+            dict->RemoveKey(PdfName("DA"));
+            dict->AddKey(PdfName("DA"), PdfString(newDaString));
+            
+            delete newDaStringSplits;
+            delete daStringSplits;
+        }
+        
+        delete fields;
+        
+        std::vector<PdfAnnotation*>* annots = PiPiUtil::SearchFieldAnnotation(document, fieldName);
+        
+        for (auto iterator = annots->begin(); iterator != annots->end(); iterator.operator++()) {
+            PdfAnnotation* annot = *iterator;
+            
+            PiPiAppearanceUtil::GenerateAppearance(fontManager, annot);
+        }
+        
+        delete annots;
+        
+        return this;
+    }
+
+    PiPiEditor* PiPiEditor::setFieldFontSize(std::string fieldName, float fontSize) {
+        return this;
+    }
 }
--- src/pipi/PiPiEditor.h
+++ src/pipi/PiPiEditor.h
@@ -3,26 +3,42 @@
 #include <iostream>
 #include "podofo/podofo.h"
 #include "PiPiUtil.h"
+#include "PiPiAppearanceUtil.h"
 #include "PiPiCommon.h"
+#include "PiPiFontManager.h"
+#include "PiPiTextHorizontalAlignment.h"
 
 using namespace PoDoFo;
 
 namespace PiPi {
 	class PiPiEditor {
 		public:
-			PiPiEditor(PdfMemDocument* document);
+			PiPiEditor(PdfMemDocument* document, PiPiFontManager* fontManager);
 
             PiPiEditor* flatten();
 			PiPiEditor* flatten(std::string fieldName);
+        
             PiPiEditor* addField(std::string fieldName, PiPiFieldType type, unsigned int page, double x, double y, double width, double height);
 			PiPiEditor* removeField(std::string fieldName);
 			PiPiEditor* renameField(std::string oldFieldName, std::string newFieldName);
 
+            PiPiEditor* setFieldFontName(std::string fieldName, std::string fontName);
+            PiPiEditor* setFieldFontSize(std::string fieldName, float fontSize);
+            
+            PiPiEditor* setFieldTextHorizontalAlignment(std::string fieldName, PiPiTextHorizontalAlignment alignment);
+        
+            PiPiEditor* setFieldColor(std::string fieldName, int red, int green, int blue);
+            PiPiEditor* setFieldBackgroundColor(std::string fieldName, int red, int green, int blue);
+            PiPiEditor* setFieldBorderColor(std::string fieldName, int red, int green, int blue);
+        
+            PiPiEditor* setFieldMultiline(std::string fieldName, bool multiline);
+        
 			bool isOperable();
 
 		private:
-			void init(PdfMemDocument* document);
+			void init(PdfMemDocument* document, PiPiFontManager* fontManager);
 
 			PdfMemDocument* document;
+            PiPiFontManager* fontManager;
 	};
 }
