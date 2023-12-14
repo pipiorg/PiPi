2e6bfb007009586a2d71cbe3551386ae5cfce651
CH-Chang <ch.chang.software@gmail.com>
2023-12-14 16:23:04 +0800

    feat: add utfcpp lib

---
+++ cmake/utfcpp.cmake
@@ -0,0 +1,19 @@
+include(FetchContent)
+
+set(UTFCPP_GIT_TAG		v4.0.4)
+set(UTFCPP_GIT_URL		https://github.com/nemtrif/utfcpp.git)
+set(UTFCPP_SOURCE_DIR	${CMAKE_SOURCE_DIR}/ext/utfcpp)
+set(UTFCPP_BINARY_DIR	${CMAKE_SOURCE_DIR}/ext-build/utfcpp)
+
+set(UTF8_TESTS OFF CACHE BOOL "" FORCE)
+set(UTF8_SAMPLES OFF CACHE BOOL "" FORCE)
+
+FetchContent_Declare(
+	utfcpp
+	GIT_REPOSITORY 	${UTFCPP_GIT_URL}
+	GIT_TAG			${UTFCPP_GIT_TAG}
+	SOURCE_DIR		${UTFCPP_SOURCE_DIR}
+	BINARY_DIR		${UTFCPP_BINARY_DIR}
+)
+
+FetchContent_MakeAvailable(utfcpp)
